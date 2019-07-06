e10e6ba81f4d06dee6be1a4c37fe35f06fc27fe8
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-07-06 21:04:35 +0300

    Update main.c

--- letpr/main.c
+++ letpr/main.c
@@ -9,15 +9,24 @@ int main(void)
 	srand(time(NULL));
 
 	char line[MAXLINE];
-	gets(line);
-	int pogr = 0 + rand() % 20;
+	int kolvo = 0;
 	int ish;
 	int mist;
+	int pogr;
+
+	gets(line);
+
+	for (ish = 0; line[ish] == '1' || line[ish] == '0' || ish == 999; ish++)
+	{
+		kolvo++;
+	}
+
+	pogr = 0 + rand() % (kolvo *40/100);
 
 	do
 	{
 		mist = 0 + rand() % 2;
-		ish = 0 + rand() % 20;
+		ish = 0 + rand() % (kolvo-1);
 		pogr = pogr--;
 
 		if (mist == 1)

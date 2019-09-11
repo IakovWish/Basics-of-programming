b43766a2b0d3b36307b89b072939ab0dad6e0562
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-09-11 11:19:59 +0300

    Update main.c

--- lab8/main.c
+++ lab8/main.c
@@ -10,34 +10,34 @@ void process_line(char buffer[]); // объявляем функцию
 int main(void)
 {
 	char line[MAXLINE];
-	printf ("Please, enter some words\nWARNING: words which consist of more than N letters will be deleted\n");
-	gets (line);
-	printf ("Enter N=");
-	scanf ("%d", &N);
-	process_line (line);
-	printf ("Result:\n");
-	puts (line);
+	printf("Please, enter some words\nWARNING: words which consist of more than N letters will be deleted\n");
+	gets(line);
+	printf("Enter N=");
+	scanf("%d", &N);
+	process_line(line);
+	printf("Result:\n");
+	puts(line);
 	return 0;
 }
 
-void process_line (char buffer[])
+void process_line(char buffer[])
 {
 	/*объявление и инициализация*/
 	char c; // текущий символ
 	int word = NO; // признак слова
 	int overkill = NO; // индикатор перебора букв в слове
 	int symb = NO; // индикатор наличия лишних символов
-	char *in_ptr = buffer; // указатель на текущий символ входной строки
-	char *out_ptr = buffer; // указатель на текущий символ выходной строки
-	char *word_ptr = buffer; // указатель на начало слова
+	char* in_ptr = buffer; // указатель на текущий символ входной строки
+	char* out_ptr = buffer; // указатель на текущий символ выходной строки
+	char* word_ptr = buffer; // указатель на начало слова
 
 	do
 	{
 		c = *in_ptr; // взять текущий символ из буфера
-		if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0' || c == '?' || c == '!' || c == ';' || c == ':' || 
+		if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0' || c == '?' || c == '!' || c == ';' || c == ':' ||
 			c == '-' || c == '_' || c == '(' || c == ')' || c == '\t' || c == '/' || c == '&' || c == '"') // разделитель найден
 		{
-			if (symb == NO && word == YES && overkill == NO || symb == YES && word == YES) // если подходит по-1му/2му условию выполнить...
+			if (symb == NO && word == YES && overkill == NO || symb == YES) // если подходит по-1му/2му условию выполнить...
 			{
 				while (word_ptr < in_ptr)  // слово не подлежит удалению оно копируется в результирующую строку
 				{
@@ -68,6 +68,5 @@ void process_line (char buffer[])
 			word = YES;
 		}
 		in_ptr++;
-	}
-	while (c != '\0'); // продолжать до конца строки
+	} while (c != '\0'); // продолжать до конца строки
 }

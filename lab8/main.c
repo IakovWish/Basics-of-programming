0510e2c0e4de205ada646d303815e26d10533821
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-09-16 18:49:07 +0300

    Update main.c

--- lab8/main.c
+++ lab8/main.c
@@ -1,26 +1,28 @@
 #pragma warning(disable : 4996) //необходимо для использования устаревших функций (scanf)
 #include <stdio.h>
+
+
 /*определение символических констант*/
 #define YES 1
 #define NO 0
 #define MAXLINE 1000
-int N; // наибольшее количество букв в строке
-void process_line(char buffer[]); // объявляем функцию
+void process_line(char buffer[], int N); // объявляем функцию
 
 int main(void)
 {
+	int N; // наибольшее количество букв в строке
 	char line[MAXLINE];
 	printf("Please, enter some words\nWARNING: words which consist of more than N letters will be deleted\n");
 	gets(line);
 	printf("Enter N=");
 	scanf("%d", &N);
-	process_line(line);
-	printf("Result:\n");
+	process_line(line, N);
+	printf("Result: %s\n", line);
 	puts(line);
 	return 0;
 }
 
-void process_line(char buffer[])
+void process_line(char buffer[], int N)
 {
 	/*объявление и инициализация*/
 	char c; // текущий символ
@@ -30,43 +32,53 @@ void process_line(char buffer[])
 	char* in_ptr = buffer; // указатель на текущий символ входной строки
 	char* out_ptr = buffer; // указатель на текущий символ выходной строки
 	char* word_ptr = buffer; // указатель на начало слова
+	int k;
 
-	do
+	if (strlen(buffer) != 0)
 	{
-		c = *in_ptr; // взять текущий символ из буфера
-		if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0' || c == '?' || c == '!' || c == ';' || c == ':' ||
-			c == '-' || c == '_' || c == '(' || c == ')' || c == '\t' || c == '/' || c == '&' || c == '"') // разделитель найден
+		c = *in_ptr;
+		while (c != '\0')
 		{
-			if (symb == NO && word == YES && overkill == NO || symb == YES) // если подходит по-1му/2му условию выполнить...
+			c = *in_ptr; // взять текущий символ из буфера
+			if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0' || c == '?' || c == '!' || c == ';' || c == ':' ||
+				c == '-' || c == '_' || c == '(' || c == ')' || c == '\t' || c == '/' || c == '&' || c == '"') // разделитель найден
 			{
-				while (word_ptr < in_ptr)  // слово не подлежит удалению оно копируется в результирующую строку
+				if (symb == NO && word == YES && overkill == NO || symb == YES) // если подходит по-1му/2му условию выполнить...
 				{
-					*out_ptr++ = *word_ptr++;  // копирование слова
+					while (word_ptr < in_ptr)  // слово не подлежит удалению оно копируется в результирующую строку
+					{
+						*out_ptr++ = *word_ptr++;  // копирование слова
+					}
 				}
-			}
-			*out_ptr++ = c; // вставляем разделитель
-			
-			word = NO;
-			symb = NO;
-			overkill = NO;
-		}
+				*out_ptr++ = c; // вставляем разделитель
 
-		else
-		{
-			if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z')) // проверить не является ли этот символ лишним
-			{
-				symb = YES; // в слове есть лишний символ
+				word = NO;
+				symb = NO;
+				overkill = NO;
 			}
-			if (word == NO) // найдена первая буква слова
+
+			else
 			{
-				word_ptr = in_ptr; // запомнить адрес начала слова
+				if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z')) // проверить не является ли этот символ лишним
+				{
+					symb = YES; // в слове есть лишний символ
+				}
+				if (word == NO) // найдена первая буква слова
+				{
+					word_ptr = in_ptr; // запомнить адрес начала слова
+				}
+				if (in_ptr - word_ptr + 1 > N) // проверка на <перебор> букв
+				{
+					overkill = YES; // перебор
+				}
+				word = YES;
 			}
-			if (in_ptr - word_ptr + 1 > N) // проверка на <перебор> букв
-			{
-				overkill = YES; // перебор
-			}
-			word = YES;
-		}
-		in_ptr++;
-	} while (c != '\0'); // продолжать до конца строки
+			in_ptr++;
+		} //while (c != '\0'); // продолжать до конца строки
+	}
+	else
+	{
+		printf("error\n"); // выводим ошибку
+		exit(1);
+	}
 }

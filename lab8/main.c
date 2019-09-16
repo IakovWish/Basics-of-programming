51ae911295d0b31d695f5ec2b58ee1b1093868c2
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-09-16 20:59:33 +0300

    Update main.c

--- lab8/main.c
+++ lab8/main.c
@@ -1,7 +1,6 @@
 #pragma warning(disable : 4996) //необходимо для использования устаревших функций (scanf)
 #include <stdio.h>
 
-
 /*определение символических констант*/
 #define YES 1
 #define NO 0

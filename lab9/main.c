24f028b864257e723e1e50802da1227b433945fa
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-09-22 15:28:32 +0300

    Update main.c
    полностью рабочая версия на данный момент

--- lab9/main.c
+++ lab9/main.c
@@ -1,102 +1,132 @@
 #pragma warning(disable : 4996) //необходимо для использования устаревших функций (scanf)
 #include <stdio.h>
+
 /*определение символических констант*/
 #define YES 1
 #define NO 0
-#define MAXLINE 1000
-void process_line(char buffer[]); // объявляем функцию
+#define MAXLINE 1024
 
 int main(void)
 {
-	char line[MAXLINE];
-	printf("Please, enter some words\n");
-	gets(line);
-	process_line(line);
-	printf("Result:\n");
-	puts(line);
-	return 0;
-}
-
-void process_line(char buffer[])
-{
 	/*объявление и инициализация*/
-	char c = 'a'; // текущий символ
+	char line[MAXLINE]; // текущая строка
+	char result[MAXLINE];
+	char c; // текущий символ
 	int word = NO; // признак слова
 	int symb = NO; // индикатор наличия лишних символов
-	char* in_ptr = buffer; // указатель на текущий символ входной строки
-	char* out_ptr = buffer; // указатель на текущий символ выходной строки
-	char* word_ptr = buffer; // указатель на начало слова
+	char* in_ptr = line; // указатель на текущий символ входной строки
+	char* out_ptr = result; // указатель на текущий символ выходной строки
+	char* word_ptr = line; // указатель на начало слова
+	char* output_ptr = result;
 	int cnt = 0; // счетчик
 	int cnt_max = 0;
-
-	do
+	FILE* fpin;
+	FILE* fpout;
+	
+	fpin = fopen("file.txt", "rt"); // открыть файл для чтения
+	if (fpin == NULL)
 	{
+		return; // ошибка при открытии файла
+	}
+
+	fpout = fopen("result.txt", "wt"); // открыть файл для записи
+	if (fpout == NULL)
+	{
+		return; // ошибка при открытии файла
+	}
+
+	while (!feof(fpin))// цикл до конца файла
+	{
+		cnt_max = 0;
+		in_ptr = fgets(line, MAXLINE, fpin);
+		if (in_ptr == NULL)
+		{
+			break; // файл исчерпан
+		}
 		do
 		{
-			do
+			c = *in_ptr; // взять текущий символ из буфера
+			if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0' || c == '?' || c == '!' || c == ';' || c == ':' ||
+				c == '-' || c == '_' || c == '(' || c == ')' || c == '\t' || c == '/' || c == '&' || c == '"') // разделитель найден
 			{
-				c = *in_ptr; // взять текущий символ из буфера
-				if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && c != ' ' && c != '\0' && c != '.' && c != ',' && c != '\n' && c != '?' &&
-					c != '!' && c != ';' && c != ':' && c != '-' && c != '_' && c != '\t' && c != ')' && c != '(' && c != '/' && c != '&' && c != '"')
+				if (cnt > cnt_max && symb == NO)
+				{
+					cnt_max = cnt;
+				}
+				word = NO;
+				symb = NO;
+				cnt = 0;
+			}
+			else
+			{
+				cnt++;
+				if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z')) // проверить не является ли этот символ лишним
 				{
 					symb = YES; // в слове есть лишний символ
 				}
 				if (word == NO) // найдена первая буква слова
 				{
-					word_ptr = in_ptr; // запомнить адресс начала слова
+					word_ptr = in_ptr; // запомнить адрес начала слова
 				}
 				word = YES;
-				in_ptr++; // смотрим следущую букву
-				cnt++;
-			} while (c != ' ' && c != '\0' && c != '.' && c != ',' && c != '\n' && c != '?' && c != '!' && c != ';' && c != ':' &&
-				c != '-' && c != '_' && c != '\t' && c != ')' && c != '(' && c != '/' && c != '&' && c != '"'); // повторяем до конца слова
-
-			if (cnt > cnt_max && symb == NO)
-			{
-				cnt_max = cnt;
 			}
-			cnt = 0;
-			word = NO;
-			symb = NO;
+			in_ptr++;
 		} while (c != '\0');
 
-		in_ptr = buffer;
-
+		in_ptr = line;
 
 		do
 		{
-			do
+			c = *in_ptr; // взять текущий символ из буфера
+			if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0' || c == '?' || c == '!' || c == ';' || c == ':' ||
+				c == '-' || c == '_' || c == '(' || c == ')' || c == '\t' || c == '/' || c == '&' || c == '"') // разделитель найден
 			{
-				c = *in_ptr; // взять текущий символ из буфера
-				if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && c != ' ' && c != '\0' && c != '.' && c != ',' && c != '\n' && c != '?' && // ищем лишние символы
-					c != '!' && c != ';' && c != ':' && c != '-' && c != '_' && c != '\t' && c != ')' && c != '(' && c != '/' && c != '&' && c != '"')
+				if (word == YES && symb == NO && cnt != cnt_max || symb == YES)
+				{
+					while (word_ptr < in_ptr)
+					{
+						*out_ptr++ = *word_ptr++; // копирование слова
+					}
+					if (c != '\0')
+					{
+						*out_ptr++ = c;
+					}
+				}
+				else
+				{
+					while (word_ptr++ < in_ptr)
+					{}
+					if (c != '\0')
+					{
+						*out_ptr++ = c;
+					}
+				}
+				word = NO;
+				symb = NO;
+				cnt = 0;
+			}
+			else
+			{
+				cnt++;
+				if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z')) // проверить не является ли этот символ лишним
 				{
 					symb = YES; // в слове есть лишний символ
 				}
 				if (word == NO) // найдена первая буква слова
 				{
-					word_ptr = in_ptr; // запомнить адресс начала слова
+					word_ptr = in_ptr; // запомнить адрес начала слова
 				}
 				word = YES;
-				in_ptr++; // смотрим следущую букву
-				cnt++;
-			} while (c != ' ' && c != '\0' && c != '.' && c != ',' && c != '\n' && c != '?' && c != '!' && c != ';' && c != ':' &&
-				c != '-' && c != '_' && c != '\t' && c != ')' && c != '(' && c != '/' && c != '&' && c != '"'); // повторяем до конца слова
-
-			if (word = YES && symb == NO && cnt != cnt_max || symb == YES)
-			{
-				while (word_ptr < in_ptr)
-				{
-					*out_ptr++ = *word_ptr++; // копирование слова
-				}
 			}
-			cnt = 0;
-			word = NO;
-			symb = NO;
+			in_ptr++;
+		} while (c != '\0');
+	}
 
-		} while (c != '\0'); // продолжать до конца строки
-
-		*out_ptr++ = c;
-
-	} while (c != '\0'); // продолжать до конца строки
+	while (output_ptr != out_ptr)
+	{
+		fprintf(fpout, "%c", *output_ptr++);
+	}
+	fclose(fpin); // закрыть входной файл
+	fclose(fpout); // закрыть выходной файл
+	return 0;
 }

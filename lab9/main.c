cf5fcc23c58884c649a2afcee32c3428f0a6f5e3
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-09-22 18:18:50 +0300

    Update main.c
    дописываем комментарии

--- lab9/main.c
+++ lab9/main.c
@@ -4,41 +4,42 @@
 /*определение символических констант*/
 #define YES 1
 #define NO 0
-#define MAXLINE 1024
+#define MAXLINE 1024  // максимальная длина массива
 
 int main(void)
 {
 	/*объявление и инициализация*/
 	char line[MAXLINE]; // текущая строка
-	char result[MAXLINE];
+	char result[MAXLINE]; // результирующая строка
 	char c; // текущий символ
 	int word = NO; // признак слова
 	int symb = NO; // индикатор наличия лишних символов
+	int cnt = 0; // счетчик
+	int cnt_max; // количество букв в самом длинном слове в текущей строке
 	char* in_ptr = line; // указатель на текущий символ входной строки
 	char* out_ptr = result; // указатель на текущий символ выходной строки
 	char* word_ptr = line; // указатель на начало слова
-	char* output_ptr = result;
-	int cnt = 0; // счетчик
-	int cnt_max = 0;
-	FILE* fpin;
-	FILE* fpout;
+	char* output_ptr = result; // указатель на результирующий массив
+
+	FILE* fpin = fopen("file.txt", "rt"); // открыть файл для чтения
+	FILE* fpout = fopen("result.txt", "wt"); // открыть файл для записи
 	
-	fpin = fopen("file.txt", "rt"); // открыть файл для чтения
 	if (fpin == NULL)
 	{
+		printf("error opening file\n"); // информация об ошибке
 		return; // ошибка при открытии файла
 	}
 
-	fpout = fopen("result.txt", "wt"); // открыть файл для записи
 	if (fpout == NULL)
 	{
+		printf("error opening file\n"); // информация об ошибке
 		return; // ошибка при открытии файла
 	}
 
 	while (!feof(fpin))// цикл до конца файла
 	{
-		cnt_max = 0;
-		in_ptr = fgets(line, MAXLINE, fpin);
+		cnt_max = 0; // перед рассмотрением каждой строки аннулируем количество букв в самом длинном слове
+		in_ptr = fgets(line, MAXLINE, fpin); // перезаписываем данные из файла в 
 		if (in_ptr == NULL)
 		{
 			break; // файл исчерпан

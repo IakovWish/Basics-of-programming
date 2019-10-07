4ae26013fe26d815d6cd73e6bafaa93c736fe473
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-10-07 18:09:25 +0300

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -1,145 +1,75 @@
 #pragma warning(disable : 4996) //необходимо для использования устаревших функций (scanf)
 #include <stdio.h>
-
-/*определение символических констант*/
+/* определение символических констант*/
 #define YES 1
 #define NO 0
-#define MAXLINE 1024  // максимальная длина массива
-
-/*объявляем функцию*/
-void cnt_max_finder(); // ищем самое длинное слово в строке
-void deliter(); // удаляем самое длинное слово в строке
-
-/*объявление и инициализация*/
-char line[MAXLINE]; // текущая строка
-char result[MAXLINE]; // результирующая строка
-char c; // текущий символ
-int word = NO; // признак слова
-int symb = NO; // индикатор наличия лишних символов
-int cnt = 0; // счетчик
-int cnt_max; // количество букв в самом длинном слове в текущей строке
-char* in_ptr = line; // указатель на текущий символ входной строки
-char* out_ptr = result; // указатель на текущий символ выходной строки
-char* word_ptr = line; // указатель на начало слова
+#define MAXLINE 1000
+int N; // наибольшее количество букв в строке
+void process_line(char buffer[]); // объявляем функцию
 
 int main(void)
 {
-	char* output_ptr = result; // указатель на результирующий массив
-	FILE* fpin = fopen("file.txt", "rt"); // открыть файл для чтения
-	FILE* fpout = fopen("result.txt", "wt"); // открыть файл для записи
-
-	if (fpin == NULL)
-	{
-		printf("error opening file\n"); // информация об ошибке
-		return; // ошибка при открытии файла
-	}
-
-	if (fpout == NULL)
-	{
-		printf("error opening file\n"); // информация об ошибке
-		return; // ошибка при открытии файла
-	}
-
-	while (!feof(fpin))// цикл до конца файла
-	{
-		in_ptr = fgets(line, MAXLINE, fpin); // получаем новую строку и ставим на нее указатель
-
-		if (in_ptr == NULL)
-		{
-			break; // файл исчерпан
-		}
-
-		cnt_max_finder(); // ищем самое длинное слово в строке
-		deliter(); // удаляем самое длинное слово в строке
-	}
-
-	while (output_ptr != out_ptr)
-	{
-		fprintf(fpout, "%c", *output_ptr++);
-	}
-
-	fclose(fpin); // закрыть входной файл
-	fclose(fpout); // закрыть выходной файл
-
+	char line[MAXLINE];
+	printf("Please, enter some words\nWARNING: words which consist of more than N letters will be deleted\n");
+	gets(line);
+	printf("Enter N=");
+	scanf("%d", &N);
+	process_line(line);
+	printf("Result:\n");
+	puts(line);
 	return 0;
 }
 
-void cnt_max_finder() // ищем самое длинное слово в строке
+void process_line(char buffer[])
 {
-	cnt_max = 0; // перед рассмотрением каждой строки аннулируем количество букв в самом длинном слове
-	do
-	{
-		c = *in_ptr; // взять текущий символ из буфера
-		if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0' || c == '?' || c == '!' || c == ';' || c == ':' ||
-			c == '-' || c == '_' || c == '(' || c == ')' || c == '\t' || c == '/' || c == '&' || c == '"') // разделитель найден
-		{
-			if (cnt > cnt_max && symb == NO)
-			{
-				cnt_max = cnt;
-			}
-			word = NO;
-			symb = NO;
-			cnt = 0;
-		}
-		else
-		{
-			cnt++;
-			if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z')) // проверить не является ли этот символ лишним
-			{
-				symb = YES; // в слове есть лишний символ
-			}
-			if (word == NO) // найдена первая буква слова
-			{
-				word_ptr = in_ptr; // запомнить адрес начала слова
-			}
-			word = YES;
-		}
-		in_ptr++;
-	} while (c != '\0');
-}
+	char c; // текущий символ
+	int word = NO; // признак слова
+	int overkill = NO; // инд перебора букв в слове
+	int symb = NO; // индикатор наличия лишних символов
+	int ish = 0; // позиция текущего символа исходной cтроки
+	int res = 0; // позиция текущего символа результирующей строки
+	int start = 0; // позиция начала слова
+	int n = 0; // сщетчик букв в слове
+	int j;
 
-void deliter() // удаляем самое длинное слово в строке
-{
-	in_ptr = line;
 	do
 	{
-		c = *in_ptr; // взять текущий символ из буфера
+		c = buffer[ish]; // взять текущий символ из буфера
 		if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0' || c == '?' || c == '!' || c == ';' || c == ':' ||
 			c == '-' || c == '_' || c == '(' || c == ')' || c == '\t' || c == '/' || c == '&' || c == '"') // разделитель найден
 		{
-			if (word == YES && symb == NO && cnt != cnt_max || symb == YES)
+			if (symb == NO && word == YES && overkill == NO || symb == YES) // если подходит по-1му/2му условию выполнить...
 			{
-				while (word_ptr < in_ptr)
+				for (j = start; j < ish; j++) // слово не подлежит удалению оно копируется в результирующую строку вместе со своим разделителем
 				{
-					*out_ptr++ = *word_ptr++; // копирование слова
+					buffer[res++] = buffer[j]; // копирование слова
 				}
 			}
-			else
-			{
-				word_ptr = word_ptr + cnt_max;
-			}
 
-			if (c != '\0')
-			{
-				*out_ptr++ = c;
-			}
 			word = NO;
 			symb = NO;
-			cnt = 0;
+			overkill = NO;
+			n = 0;
+			buffer[res++] = c; // ставим разделитель
 		}
+
 		else
 		{
-			cnt++;
+			n++; // найдена новая буква, увеличиваем счетчик букв
 			if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z')) // проверить не является ли этот символ лишним
 			{
 				symb = YES; // в слове есть лишний символ
 			}
 			if (word == NO) // найдена первая буква слова
 			{
-				word_ptr = in_ptr; // запомнить адрес начала слова
+				start = ish; // запомнить позицию начала слова
+			}
+			if (n > N) // проверка на перебор букв
+			{
+				overkill = YES; // перебор
 			}
 			word = YES;
 		}
-		in_ptr++;
-	} while (c != '\0');
+		ish++;
+	} while (c != '\0'); // продолжать до конца строки
 }

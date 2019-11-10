b3f54dcd89939b72a4b76dcda56f6b7316eee06f
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-11-10 15:34:08 +0300

    Update main.c

--- additionally/manual10/main.c
+++ additionally/manual10/main.c
@@ -1,3 +1,4 @@
+#pragma warning(disable : 4996)
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
@@ -8,40 +9,37 @@
 #define MAXLEN 1000 // максимальная длина строки
 #define NLINES 1000 // максимальное число строк
 
-int word_counter0 (char lines[][MAXLEN], int n);
-int word_counter1 (char** lines, int n);
+int word_counter0(char lines[][MAXLEN], int n);
+int word_counter1(char** lines, int n);
 
 int main(void)
 {
-	FILE* fp; // входной файл
-	int nlines0, nlines1; // число строк в файле
+	FILE* fp = fopen("C:\\Users\\User\\source\\repos\\file.txt", "rt"); // открыть входной файл для чтения
+	int nlines0 = 0; // число строк в файле
+	int nlines1 = 0; // число строк в файле
 	static char lines[NLINES][MAXLEN]; // статический массив для хранения строк
 	char** lines_ptr = NULL; // указатель на динамический массив
-	int npointers; // число указателей в динамическом массиве
+	int npointers = NLINES; // число указателей в динамическом массиве
 	int i;
 	int nwords0, nwords1; // число слов в массиве строк
-	fp = fopen("test.txt", "rt"); // открыть файл для чтения
+	
 	if (fp == NULL)
 	{
 		return; // ошибка при открытии файла
 	}
-	// заказать и обнулить динамический массив указателей (два варианта)
+	
 #if 1
-	lines_ptr = (char**)malloc(NLINES * sizeof(char*));
-	// обнулить массив указателей
-	memset(lines_ptr, 0, NLINES * sizeof(char*));
+	lines_ptr = (char**)malloc(NLINES * sizeof(char*)); // заказать динамический массив указателей
+	memset(lines_ptr, 0, NLINES * sizeof(char*)); // обнулить массив указателей
+
 #else
-	lines_ptr = (char**)calloc(NLINES, sizeof(char*));
+	lines_ptr = (char**)calloc(NLINES, sizeof(char*)); //2й вариант
+
 #endif
-	npointers = NLINES;
-	nlines0 = 0;
-	nlines1 = 0;
 	while (!feof(fp)) // цикл до конца файла
 	{
 		char line[MAXLEN];
-		// чтение строки
-		char* ptr = fgets(line, MAXLEN, fp);
-		int len;
+		char* ptr = fgets(line, MAXLEN, fp); // чтение строки
 
 		if (ptr == NULL)
 		{
@@ -55,27 +53,25 @@ int main(void)
 			strcpy(&lines[nlines0][0], line);
 			nlines0++;
 		}
-		
-		len = (int)strlen(line); // определить длину строки
+
+		int len = (int)strlen(line); // определить длину строки
 		if (nlines1 == npointers) // динамический массив исчерпан
 		{
 			npointers += NLINES;// увеличить число указателей в массиве
-			
+
 			lines_ptr = (char**)realloc(lines_ptr, npointers * sizeof(char*)); // перезаказать память для массива указателей
 			memset(&lines_ptr[nlines1], 0, NLINES * sizeof(char*)); // обнулить новую часть массива указателе
 		}
-		// заказать в памяти место для строки
-		// плюс один байт
-		lines_ptr[nlines1] = (char*)malloc(len + 1);
-		// сохранить строку в динамическом массиве
-		strcpy(lines_ptr[nlines1], line);
+		
+		lines_ptr[nlines1] = (char*)malloc(len + 1); // заказать в памяти место для строки плюс один байт
+		strcpy(lines_ptr[nlines1], line); // сохранить строку в динамическом массиве
 		nlines1++;
 	}
 	nwords0 = word_counter0(lines, nlines0);
 	nwords1 = word_counter1(lines_ptr, nlines1);
 	printf("number of words: %d\n", nwords0);
 	printf("number of words: %d\n", nwords1);
-	
+
 	for (i = 0; i < nlines1; i++) // отказаться от памяти для строк
 	{
 		if (lines_ptr[i] != NULL)
@@ -83,7 +79,7 @@ int main(void)
 			free(lines_ptr[i]);
 		}
 	}
-	
+
 	if (lines_ptr != NULL) // отказаться от динамического массива указателей
 	{
 		free(lines_ptr);
@@ -96,14 +92,11 @@ int main(void)
 int word_counter0(char lines[][MAXLEN], int n)
 {
 	int i, j;
-	int counter; // счётчик слов
-	int flag; // признак слова
-	counter = 0;
-	flag = NO;
+	int counter = 0; // счётчик слов
+	int flag = NO; // признак слова
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++) // цикл по строкам
 	{
-		// цикл по строкам
 		char* ptr = &lines[i][0]; // указатель на текущую строку
 
 		for (j = 0; ptr[j] != '\0'; j++)
@@ -134,20 +127,17 @@ int word_counter0(char lines[][MAXLEN], int n)
 int word_counter1(char** lines, int n)
 {
 	int i, j;
-	int counter; // счётчик слов
-	int flag; // признак слова
-	counter = 0;
-	flag = NO;
+	int counter = 0; // счётчик слов
+	int flag = NO; // признак слова
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++) // цикл по строкам
 	{
-		// цикл по строкам
-		// получить адрес текущей строки
-		// ( два варианта )
 #if 1
 		char* ptr = *(lines + i); // указатель на текущую строку
+
 #else
-		char* ptr = lines[i]; // указатель на текущую строку
+		char* ptr = lines[i]; // 2й вариант
+
 #endif
 		for (j = 0; ptr[j] != '\0'; j++)
 		{
@@ -157,7 +147,7 @@ int word_counter1(char** lines, int n)
 			}
 			else // текущий символ - буква
 			{
-				
+
 				if (flag == NO) // первая буква в слове
 				{
 					counter++;
